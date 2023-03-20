25dbe77eb22d5f7c238f0e405b1ea4aaf8cec5ff
Lucasvgb <lucasvgb@id.uff.br>
2023-03-20 20:37:18 -0300

    triangulos

---
+++ medidasTriangulos/triangulos/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner input = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+
+
+
+        input.close();
+    }
+}
