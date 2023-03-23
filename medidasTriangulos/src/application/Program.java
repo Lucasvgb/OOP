30357af8eabe07d649658fc7c503dae713c0c660
Lucasvgb <lucasvgb@id.uff.br>
2023-03-23 12:08:35 -0300

    implementando POO

--- medidasTriangulos/src/application/Program.java
+++ medidasTriangulos/src/application/Program.java
@@ -3,30 +3,33 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Program {
     public static void main(String[] args) {
       
       Scanner input = new Scanner(System.in);
       Locale.setDefault(Locale.US);
 
-      double xA, xB, xC; //Triângulo X
-      double yA, yB, yC; //Triângulo Y
+      Triangle x,y;
+      x = new Triangle();
+      y = new Triangle();
 
       System.out.println("Entre com as medidas do triangulo X: ");
-      xA = input.nextDouble();
-      xB = input.nextDouble();
-      xC = input.nextDouble();
+      x.a = input.nextDouble();
+      x.b = input.nextDouble();
+      x.c = input.nextDouble();
 
       System.out.println("Entre com as medidas do triangulo Y: ");
-      yA = input.nextDouble();
-      yB = input.nextDouble();
-      yC = input.nextDouble();
+      y.a = input.nextDouble();
+      y.b = input.nextDouble();
+      y.c = input.nextDouble();
 
-      double p = (xA + xB + xC) / 2.0;
-      double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC)); // FormulaParaCalcularArea Triângulo X
+      double p = (x.a + x.b + x.c) / 2.0;
+      double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c)); // FormulaParaCalcularArea Triângulo X
 
-      p = (yA + yB + yC) / 2.0;
-      double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC)); // FormulaParaCalcularArea Triângulo Y
+      p = (y.a + y.b + y.c) / 2.0;
+      double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c)); // FormulaParaCalcularArea Triângulo Y
       
       System.out.printf("Area do triangulo X: %.4f%n", areaX);
       System.out.printf("Area do triangulo Y: %.4f%n", areaY);
